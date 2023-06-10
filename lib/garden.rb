24a8c8981b825075f07b110fe0fadc5aedf5014b
Meshack Korir <meshackkorir100@gmail.com>
2023-06-10 07:57:02 +0300

    building

--- lib/garden.rb
+++ lib/garden.rb
@@ -1,25 +1,43 @@
-# Something is missing here
-require_relative './plant'
-class Garden
-  attr_accessor :name
+# # Something is missing here
+# require_relative './plant'
+# class Garden
+#   attr_accessor :name
 
-  def initialize(name:)
-    @name = name
-  end
+#   def initialize(name:)
+#     @name = name
+#   end
 
-  def plants
-    Plant.all.select do |plant|
-      plant.garden == self
-    end
+#   def plants
+#     Plant.all.select do |plant|
+#       plant.garden == self
+#     end
+#   end
+# end
+
+# lawn = Garden.new(name: 'Front Lawn')
+
+# basil = Plant.new(name: 'Basil')
+# basil.garden = lawn
+
+# cucumber = Plant.new(name: 'Cucumber')
+# cucumber.garden = lawn
+
+# p lawn.plants
+class Car
+  attr_accessor: make, model, year
+end
+  def initialize(make,model,year)
+    @make = make
+    @model = model
+    @year = year
   end
 end
 
-lawn = Garden.new(name: 'Front Lawn')
+car1 = car.new("Toyota", "Camry", "2021")
+car2 = car.new("Ford", "Accord", "2010")
+car3 = car.new("Mazda", "Fresh", "2023")
 
-basil = Plant.new(name: 'Basil')
-basil.garden = lawn
+puts car1.make, car1.model, car1.year
+puts car2.make, car2.model, car2.year
+puts car3.make, car3.model, car3.year
 
-cucumber = Plant.new(name: 'Cucumber')
-cucumber.garden = lawn
-
-p lawn.plants
